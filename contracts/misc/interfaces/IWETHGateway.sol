77458f0e806b6b1dfb16db1017bd4acdde18f4ba
cyrillicw <cyrillicw@gmail.com>
2021-02-01 23:18:19 +0200

    added funct to gateway

--- contracts/misc/interfaces/IWETHGateway.sol
+++ contracts/misc/interfaces/IWETHGateway.sol
@@ -6,15 +6,5 @@ interface IWETHGateway {
 
   function withdrawETH(uint256 amount, address onBehalfOf) external;
 
-  function repayETH(
-    uint256 amount,
-    uint256 rateMode,
-    address onBehalfOf
-  ) external payable;
-
-  function borrowETH(
-    uint256 amount,
-    uint256 interesRateMode,
-    uint16 referralCode
-  ) external;
+  function getAWETHAddress() external view returns (address);
 }
